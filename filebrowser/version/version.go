949f46b752a1aac8a33c7de4c95698d13017c2b7
niubility000 <13902099006@163.com>
2024-04-07 15:59:18 +0800

    Update version.go

--- filebrowser/version/version.go
+++ filebrowser/version/version.go
@@ -4,7 +4,7 @@ var (
 	// Version is the current File Browser version.
 
 
-	Version = "- PDJ Media Repeater-2.0"
+	Version = "- PDJ Media Repeater-2.02"
 
 	// CommitSHA is the commmit sha.
 	CommitSHA = "(unknown)"
